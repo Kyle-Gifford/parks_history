22b997718c96b4d032d979a0dce85961efca219b
Kyle Gifford <cakyle@gmail.com>
2022-10-29 09:47:02 +0000

    Clean output

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
 import requests
-# import json
 from bs4 import BeautifulSoup
 
-# url = 'https://en.wikipedia.org/wiki/Globe_Life_Field'
-# x = requests.get(url)
-# html_doc = x.text
+url = 'https://en.wikipedia.org/wiki/Globe_Life_Field'
+# url = 'https://en.wikipedia.org/wiki/Yankee_Stadium'
+result = requests.get(url)
+html_doc = result.text
 
-with open("glf.txt") as file:
-    html_doc = file.read()
+# with open("glf.txt") as file:
+#     html_doc = file.read()
 
-print(html_doc)
 
 loc = html_doc.find('<h2><span class="mw-headline" id="History">')
-print('***')
-print(loc)
-print('^^^')
+loc2 = html_doc.find('<h2>', loc+1)
+html_doc = html_doc[loc:loc2]
 
-# soup = BeautifulSoup(html_doc, 'html.parser')
-# print('hi from main')
-# all_p = soup.find_all('p')
-# para_count = 0
-# for para in all_p:
-#     if para_count > 3:
-#         break
-#     para_count += 1
-#     print(para.get_text())
-# for para_num in range(4):
-#     print(all_p[para_num].get_text())
-print('done')
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+all_p = soup.find_all('p')
+
+output = ""
+for para in all_p:
+    output += para.get_text()
+print(output)
